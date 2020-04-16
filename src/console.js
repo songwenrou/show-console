cad95b8dff39d724e40ddf754bbde3dcb60abe3a
songwenrou <songyingying@zhimadj.com>
2020-04-16 19:29:22 +0800

    feat:console 成型 开熏

--- src/console.js
+++ src/console.js
@@ -11,62 +11,129 @@ export default function consoleFun() {
         var _console = {
             log: console.log,
         };
-        let mainHtml = document.querySelector('#slog')
+        let mainHtml = document.querySelector('#slog-list');
+        let unsentList = []
+        function addList(arg){
+            let node=document.createElement("DIV");
+            unsentList.push(arg)
+            
+            if(mainHtml){
+                // node.innerHTML=JSON.stringify(arg);
+                // node.className="slog-list-item";
+                // mainHtml.appendChild(node)
+                unsentList.forEach(item => {
+                    let node=document.createElement("DIV");
+                    // _console.log(item)
+                    node.className="slog-list-item";
+                    node.innerHTML=addDom(item);
+                    node.addEventListener("click",childrenDomClick);
+                    // setTimeout(()=>{
+                    // },100)
+                    mainHtml.appendChild(node) 
+                    
+                });
+                unsentList = []
+            }
+             
+            allList.push(JSON.stringify(arg));
+            consoleList.push(JSON.stringify(arg));
+
+        }
+        function addDom (item){
+            _console.log(item)
+            return  Object.values(item).reduce((domStr, i)=>{
+                let aa = (i)=>{
+                    if(type(i) == 'Object'){
+                    
+                        return domStr +''+ Object.entries(i).reduce((itemDomStr, arr , index)=>{
+                            // return itemDomStr + '<div class="slog-list-child"><div class="slog-list-child-index">'+index+'</div>'+ arr[0] +':'+ '<em>> Object</em>' + aa(arr[1])+'</div>' 
+                            return `${itemDomStr}<div class="slog-list-child"  onclick="childrenDomClick()"><div class="slog-list-child-index">${index}</div>${arr[0]}:${aa(arr[1])}</div>`
+    
+                        },'<em> Object </em>: '+JSON.stringify(i))
+    
+                    }else{
+                        return domStr + i + '<br/>'
+                    }
+                    
+ 
+                }
+                return domStr+aa(i)
+                // if(type(i) == 'Object'){
+                    
+                //     return domStr + Object.entries(i).reduce((itemDomStr, arr , index)=>{
+                //         return itemDomStr + '<div class="slog-list-child">>&nbsp;'+index+':&nbsp;'+ arr[0] +':'+ aa(arr[1])+'</div>'
+
+                //     },'[Object] <br/>')
+
+                // }else{
+                //     return domStr + i + '<br/>'
+                // }
+                
+            },'')
+            
+        }
+        // function easyAdd(){
+            // let listDom = document.querySelector('#slog-list')
+           
+        // }
         function type (i){
             return Object.prototype.toString.call(i).slice(8,-1)
         }
         function addObjectDom(e){
 
-            let c = ``
-            for(let i in e){
-                c+=`<li>${addHtml(e[i])}</li>`
-            }
-            let d = `<div data-params="{[1,2,3]}" class="slog-item">
-                        >${type(e)}
-                        <ul style="display: none;">
-                            ${c}
-                        </ul>
-                    </div>`
+            // let c = ``
+            // for(let i in e){
+            //     c+=`<li>${addHtml(e[i])}</li>`
+            // }
+            // let d = `<div data-params="{[1,2,3]}" class="slog-item">
+            //             >${type(e)}
+            //             <ul style="display: none;">
+            //                 ${c}
+            //             </ul>
+            //         </div>`
 
                     // let node=document.createElement("DIV");
                     // let textnode=node.insertAdjacentHTML(d);
                     // node.appendChild(textnode)
                     // mainHtml.appendChild(node) 
+                    // var element = document.getElementsByTagName('p')[0];
+                    // var txt = document.createTextNode('新增文本内容'); //创建文本节点
+                    // element.appendChild(txt); //添加文本节点
         }
         function addHtml(arg){
-            for ( let item in arg){
-                _console.log(type(item))
-                // console.log(item)  
-                // type(item) 
-                // // let itemHtml='<li>'+type(arg[item])+' </li>';
-                // let node=document.createElement("LI");
-                // let textnode=document.createTextNode(type(arg[item]));
-                // node.appendChild(textnode)
-                // mainHtml.appendChild(node)
-                if (type(arg[item]) == 'Array') {
-                    addObjectDom(arg[item])
-                } else if (type(arg[item]) == 'Object') {
+            // for ( let item in arg){
+            //     _console.log(type(item))
+            //     // console.log(item)  
+            //     // type(item) 
+            //     // // let itemHtml='<li>'+type(arg[item])+' </li>';
+            //     // let node=document.createElement("LI");
+            //     // let textnode=document.createTextNode(type(arg[item]));
+            //     // node.appendChild(textnode)
+            //     // mainHtml.appendChild(node)
+            //     if (type(arg[item]) == 'Array') {
+            //         addObjectDom(arg[item])
+            //     } else if (type(arg[item]) == 'Object') {
                     
-                }else {
-                    let node=document.createElement("LI");
-                    let textnode=document.createTextNode(type(arg[item]));
-                    node.appendChild(textnode)
-                    mainHtml.appendChild(node)  
-                }
+            //     }else {
+            //         let node=document.createElement("LI");
+            //         let textnode=document.createTextNode(type(arg[item]));
+            //         node.appendChild(textnode)
+            //         mainHtml.appendChild(node)  
+            //     }
 
-            }
+            // }
         }
 
         
         console.log = function() {
-            addHtml(arguments)
+            addList(arguments)
             _console.log.apply(this, Array.prototype.slice.call(arguments, 0));
         }
         console.error = function() {
         // // 做自己的处理
-        let itemHtml='<li>111'+JSON.stringify(Object.values(arguments))+'</li>';
+        // let itemHtml='<li>111'+JSON.stringify(Object.values(arguments))+'</li>';
         // log.appendChild(itemHtml)
-        log.innerHTML=itemHtml;
+        // log.innerHTML=itemHtml;
         _console.log( JSON.stringify(Object.values(arguments)))
         // Object.values(arguments)
         // // 调用原方法输出
@@ -74,17 +141,30 @@ export default function consoleFun() {
         }
 
 
+        
+        function childrenDomClick(e){
+        let dom 
+        // console.log(e.target.className)
+        if(e.target.className == 'slog-list-item' || e.target.className == 'slog-list-child'){
+            dom = e.target;
+        }
+        // if(e.target.className == 'slog-list-child'){
+        //     dom = e.target;
+        // }
 
+        if(e.target.className == ''){
+            console.log(e.target.parentNode)
+        }
+        // console.log()
+        dom.setAttribute('showchild', dom.getAttribute("showchild") == 't'? 'f' : 't')
+        }
 
-
-        var btn = document.querySelector('.slog-item')
-        btn.addEventListener("click", function(e){
-            // document.getElementById("demo").innerHTML = "Hello World";
-            // = 'block'
-            // _console.log(e.target.children[0].style.display )
-        });
+        // var btn = document.querySelector('.slog-item')
+        // btn.addEventListener("click", function(e){
+        //     // document.getElementById("demo").innerHTML = "Hello World";
+        //     // = 'block'
+        //     // _console.log(e.target.children[0].style.display )
+        // });
         // warn、debug、error同理
     }  
-    console.log(111)
-    console.log(222)  
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,182 @@
 import consoleFun from './console.js';
+
+(()=>{
+    var cssString = `
+    .slog-bg{
+        background: rgba(0,0,0,0.5);
+        height: 100vh;
+        width: 100vw;
+        position: fixed;
+        top: 0;
+        left: 0;
+        color:#666
+    }
+    .slog-button{
+        background:#07c160;
+        box-shadow: 0px 0px 5px #07c160;
+        height:30px;
+        width:60px;
+        color:#fff;
+        margin: 0;
+        padding: 0;
+        border: 1px solid transparent;
+        outline: none;
+        position: absolute;
+        bottom: 100px;
+        right: 10px;
+        border-radius: 4px;
+        
+    }
+    .slog-list-box{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        background:#FFF;
+    }
+    .slog-list{
+        max-height: 75vh;
+        min-height:30vh;
+        width: 100%;
+        overflow-y: scroll;
+    }
+    .slog-list-item{
+        border-bottom: 1px solid #eee;
+        padding:5px;
+        dispaly:flex;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        padding-right:30px;
+    }
+    .slog-list-title{
+        display:flex;
+        border-bottom: 1px solid #9999;
+    }
+    .slog-list-title-item{
+        padding:5px 10px; 
+        border-right: 1px solid #999;
+        background:#eee;
+    }
+    .hide{
+        display:none
+    }
+    .box-hide{
+        visibility:hidden
+    }
+    .slog-list-child{
+        padding-left:20px;
+        line-height: 30px;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        display:none;
+    }
+    .slog-list-child-index:last-child{
+        border-bottom: 1px solid #ccc;
+    }
+    .slog-list-child-index {
+        width: 20px;
+        text-align: center;
+        border-left: 1px solid #ccc;
+        display: inline-block;
+        margin-right: 10px;
+        color: #ccc;
+    }
+    .slog-list-item[showchild=t] >.slog-list-child{
+        display:block
+    }
+    .slog-list-child[showchild=t] >.slog-list-child{
+        display:block
+    }
+    `
+    var style = document.createElement("style");
+    style.innerHTML = cssString
+    document.querySelector("body").appendChild(style)
+    var f = document.createElement("DIV");
+    f.setAttribute('id','slog')
+    document.querySelector("body").appendChild(f)
+
+})()
+window.slogInit = ()=>{
+let mainHtml = document.querySelector('#slog')
+let domBody = `
+        <button class="slog-button" id="slog-btn">调试</button>
+        <div class="slog-bg box-hide" id="slog-bg">
+            <div class="slog-list-box" id="slog-box">
+                <div class="slog-list-title">
+                    <div class="slog-list-title-item">all</div>
+                    <div class="slog-list-title-item">log</div>
+                    <div class="slog-list-title-item">error</div>
+                </div>
+                <div class="slog-list"id="slog-list">
+                </div>
+            </div>
+        </div>
+        
+`
+mainHtml.innerHTML=domBody
+window.allList=[]
+window.consoleList=[]
+
+// console.log(mainHtml)
+// window.onload = function(){
+    
+    var btn = document.querySelector('#slog-btn')
+    var bg = document.querySelector('#slog-bg')
+    var box = document.querySelector('#slog-box')
+    btn.addEventListener("click", function(e){
+        document.querySelector('#slog-bg').className = 'slog-bg'
+        document.querySelector('#slog-btn').className = 'slog-button hide';
+
+        easyAdd()
+    });
+    function easyAdd(){
+        let listDom = document.querySelector('#slog-list')
+        // allList.forEach(item => {
+        //     let node=document.createElement("DIV");
+        //     node.innerHTML=JSON.stringify(item);
+        //     node.className="slog-list-item";
+        //     // console.log(listDom)
+        //     setTimeout(()=>{
+        //         listDom.appendChild(node) 
+        //     },100)
+              
+        // });
+    }
+    bg.addEventListener("click", function(e){
+        document.querySelector('#slog-bg').className = 'slog-bg box-hide';
+        document.querySelector('#slog-btn').className = 'slog-button ';
+    });
+    box.addEventListener("click", function(e){
+        // console.log(e)
+        // document.querySelector('#slog-bg').className = 'slog-bg hide'
+        // document.querySelector('#slog-btn').className = 'slog-button '
+        e.stopPropagation();
+        // if(e.preventDefault){
+            
+        // }
+        // else{
+        // window.event.returnValue == false;
+        // }
+    });
+// }
 consoleFun()
+
+}
+
+
+// let mainHtml = document.querySelector('#slog')
+// // let btn=document.createElement("BUTTON");
+// // btn.innerText('调试页面');
+// var button = document.createElement("input");
+// button.setAttribute("type", "button");
+// button.setAttribute("value", 111);
+// // button.setAttribute("id", id);
+// // button.setAttribute("class", class);
+// button.style.width = "12%";
+// // button.setAttribute("onclick", "function(this.id)");
+// mainHtml.appendChild(button)
+
+// console.log(slogInit)
+// export default slogInit
